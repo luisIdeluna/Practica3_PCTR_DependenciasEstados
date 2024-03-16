42f25205b39043ea97b077c003a25cb40bc65713
LidlUbu <ldg1008@alu.ubu.es>
2024-03-16 14:05:21 +0100

    Creados los métodos comprobarAntesDeSalir y ComprobarAntesDeEntrar

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -17,15 +17,22 @@ import java.util.Hashtable;
 
 public class Parque implements IParque{
 
+	/*Requisito de capacidad del parque */
+	private static final int MAXPERSONAS =50;
 
-	// TODO 
-	private int contadorPersonasTotales;
-	private Hashtable<String, Integer> contadoresPersonasPuerta;
+	private int contadorPersonasTotales; // Total de persona dentro del parque
+
+	private Hashtable<String, Integer> contadoresPersonasPuerta; //Tabla a la que le paso una puerta y el número de personas
 	
-	
+	/**
+	 * Constructor
+	 */
 	public Parque() {
+
 		contadorPersonasTotales = 0;
+
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
+
 	}
 
 
@@ -34,17 +41,21 @@ public class Parque implements IParque{
 		
 		// Si no hay entradas por esa puerta, inicializamos
 		if (contadoresPersonasPuerta.get(puerta) == null){
+		
 			contadoresPersonasPuerta.put(puerta, 0);
+		
 		}
 		
-		// TODO
+		// Hay que comprobar antes que entrar
 				
 		
 		// Aumentamos el contador total y el individual
 		contadorPersonasTotales++;		
+		
 		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
 		
 		// Imprimimos el estado del parque
+		
 		imprimirInfo(puerta, "Entrada");
 		
 		// TODO
@@ -54,58 +65,114 @@ public class Parque implements IParque{
 		
 	}
 	
-	// 
-	// TODO Método salirDelParque
-	//
-	
-	
+	/**
+	 * 
+	 * @param puerta
+	 * @param movimiento
+	 */
 	private void imprimirInfo (String puerta, String movimiento){
+
+
 		System.out.println(movimiento + " por puerta " + puerta);
 		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
 		
 		// Iteramos por todas las puertas e imprimimos sus entradas
+
 		for(String p: contadoresPersonasPuerta.keySet()){
+
 			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
+
 		}
+
 		System.out.println(" ");
+
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	private int sumarContadoresPuerta() {
+
 		int sumaContadoresPuerta = 0;
-			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
-			while (iterPuertas.hasMoreElements()) {
-				sumaContadoresPuerta += iterPuertas.nextElement();
-			}
+
+		Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
+
+		while (iterPuertas.hasMoreElements()) {
+
+			sumaContadoresPuerta += iterPuertas.nextElement();
+
+		}
+
 		return sumaContadoresPuerta;
+
 	}
 	
 	protected void checkInvariante() {
+
 		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
+
 		// TODO 
+
 		// TODO
 		
-		
-		
+				
 	}
 
-	protected void comprobarAntesDeEntrar(){
-		//
-		// TODO
-		//
-	}
+	/**
+	 * Método compobarAntesDeEntrar
+	 * 
+	 * Se debe de comprobar que se permite el acceso de personas al parque por no llegar al áforo máximo
+	 * @throws InterruptedException 
+	 */
+	protected void comprobarAntesDeEntrar() {
 
+		
+		while (contadorPersonasTotales >= MAXPERSONAS){
+
+			try{
+
+				wait(); 
+
+			}//fin try
+
+			catch( InterruptedException e){
+
+				e.printStackTrace();
+
+			}//fin catch
+
+		}//fin while
+
+	}//fin comprobarAntesDeEntrar
+
+	//Comporbamos si existe gente dentro del parque antes de salir
 	protected void comprobarAntesDeSalir(){
-		//
-		// TODO
-		//
+		
+		while (contadorPersonasTotales <=0){
+
+			try{
+
+				wait(); 
+
+			}//fin try
+
+			catch( InterruptedException e){
+
+				e.printStackTrace();
+
+			}//fin catch
+
+		}//fin while
 	}
 
 
 	@Override
+	/**
+	 * param puerta
+	 */
 	public void salirDelParque(String puerta) {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'salirDelParque'");
 	}
 
-
-}
+}//fin clase
